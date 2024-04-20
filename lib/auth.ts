39ad89a25480f2c947fd14ea247d231233611f5e
gnasamx <gnasamx@gmail.com>
2024-04-20 22:45:35 +0530

    init

---
+++ lib/auth.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+import Resend from "next-auth/providers/resend";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Resend({
+      apiKey: process.env.AUTH_RESEND_KEY,
+      from: "onboarding@resend.dev",
+    }),
+  ],
+});
